e732f1ab288f839625adec67cfb8bdde591fc8ea
Enrique Núñez <nenrique0112@gmail.com>
2023-01-29 15:51:45 -0400

    Added XML support and comments on formatters

--- Program.cs
+++ Program.cs
@@ -2,7 +2,17 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(options =>
+{
+    // Define default input or output format
+    // options.InputFormatters.Add();
+    // options.OutputFormatters.Add();
+
+    // To specify whether te response format is accepted or not
+    // Example: Request XML response but it is not supported by the API
+    options.ReturnHttpNotAcceptable = true;  
+}).AddXmlDataContractSerializerFormatters();  // This line adds XML support to the API
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
