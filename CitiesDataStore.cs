6d1afb36659060adc83952f8e61722ec8eaf8738
Enrique Núñez <nenrique0112@gmail.com>
2023-01-28 22:04:06 -0400

    Improved architecture, Dto implemented

---
+++ CitiesDataStore.cs
@@ -0,0 +1,34 @@
+﻿using CityInfo.Api.Models;
+
+namespace CityInfo.Api;
+
+public class CitiesDataStore
+{
+    public List<CityDto> Cities { get; set; }
+    public static CitiesDataStore Current { get; set; } = new();  // Singleton
+
+    private CitiesDataStore()
+    {
+        Cities = new List<CityDto>
+        {
+            new()
+            {
+                Id = 1,
+                Name = "New York City",
+                Description = "City 1 description"
+            },
+            new()
+            {
+                Id = 2,
+                Name = "Santa Cruz de la Sierra",
+                Description = "City 2 description"
+            },
+            new()
+            {
+                Id = 3,
+                Name = "Paris",
+                Description = "City 3 description"
+            }
+        };
+    }
+}
--- Controllers/CitiesController.cs
+++ Controllers/CitiesController.cs
@@ -3,18 +3,18 @@
 namespace CityInfo.Api.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]  // Base link for this controller
+[Route("api/[controller]")] // Base link for this controller
 public class CitiesController : ControllerBase
 {
     [HttpGet]
     public JsonResult GetCities()
     {
-        return new JsonResult(
-            new List<object>
-            {
-                new { id = 1, Name = "New York City" },
-                new { id = 2, Name = "Santa Cruz de la Sierra" }
-            }
-        );
+        return new JsonResult(CitiesDataStore.Current.Cities);
+    }
+
+    [HttpGet("{id:int}")] // Path parameter
+    public JsonResult GetCity(int id)
+    {
+        return new JsonResult(CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == id));
     }
 }
