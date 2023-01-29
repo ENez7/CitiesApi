99848b7211508fe7e3221422941d5dc4454c8290
Enrique Núñez <nenrique0112@gmail.com>
2023-01-28 22:51:02 -0400

    Added .gitignore and dtos

--- CitiesDataStore.cs
+++ CitiesDataStore.cs
@@ -15,19 +15,64 @@ public class CitiesDataStore
             {
                 Id = 1,
                 Name = "New York City",
-                Description = "City 1 description"
+                Description = "City 1 description",
+                PointsOfInterest = new List<PointOfInterestDto>
+                {
+                    new ()
+                    {
+                        Id =  1,
+                        Name = "Central Park",
+                        Description = "The most visited urban park in the United States"
+                    }
+                }
             },
             new()
             {
                 Id = 2,
                 Name = "Santa Cruz de la Sierra",
-                Description = "City 2 description"
+                Description = "City 2 description",
+                PointsOfInterest = new List<PointOfInterestDto>
+                {
+                    new ()
+                    {
+                        Id =  2,
+                        Name = "Ventura Mall",
+                        Description = "Ventura Mall"
+                    },
+                    new ()
+                    {
+                        Id =  3,
+                        Name = "Las Brisas",
+                        Description = "Las Brisas"
+                    },
+                    new ()
+                    {
+                        Id =  4,
+                        Name = "Plaza 24 de septiembre",
+                        Description = "La plazanga"
+                    }
+                }
             },
             new()
             {
                 Id = 3,
                 Name = "Paris",
-                Description = "City 3 description"
+                Description = "City 3 description",
+                PointsOfInterest = new List<PointOfInterestDto>
+                {
+                    new ()
+                    {
+                        Id =  5,
+                        Name = "Eiffel Tower",
+                        Description = "The most visited tower in France"
+                    },
+                    new ()
+                    {
+                        Id =  6,
+                        Name = "Some french place",
+                        Description = "I know nothing about France and its places"
+                    },
+                }
             }
         };
     }
---
+++ Controllers/PointsOfInterestController.cs
@@ -0,0 +1,30 @@
+using CityInfo.Api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CityInfo.Api.Controllers
+{
+    [Route("api/cities/{cityId:int}/pointsofinterest")]  // This is a child of another resource (City)
+    [ApiController]
+    public class PointsOfInterestController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<PointOfInterestDto>> GetPointsOfInterest(int cityId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+            if (city == null) return NotFound();
+            return Ok(city.PointsOfInterest);
+        }
+
+        [HttpGet("pointOfInterestId:int")]
+        public ActionResult<PointOfInterestDto> GetPointOfInterest(int cityId, int pointOfInterestId)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(city => city.Id == cityId);
+            if (city == null) return NotFound();
+
+            var pointOfInterest = city.PointsOfInterest.FirstOrDefault(point => point.Id == pointOfInterestId);
+            if (pointOfInterest == null) return NotFound();
+
+            return Ok(pointOfInterest);
+        }
+    }
+}
