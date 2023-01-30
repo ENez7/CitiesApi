fb56c1c3b734c57a13efc17a0dd9b2dbcb4b5730
Enrique Núñez <nenrique0112@gmail.com>
2023-01-29 20:14:24 -0400

    Added some data annotations for validation

--- Models/PointOfInterestForCreationDto.cs
+++ Models/PointOfInterestForCreationDto.cs
@@ -1,7 +1,13 @@
-﻿namespace CityInfo.Api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CityInfo.Api.Models;
 
 public class PointOfInterestForCreationDto
 {
+    [Required(ErrorMessage = "You should provide a name for this point of interest.")]
+    [MaxLength(50)]
     public string Name { get; set; } = string.Empty;
+
+    [MaxLength(200)]
     public string? Description { get; set; }
 }
