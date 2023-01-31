b3508525250f9f130161874c1465328e1424892b
Enrique Núñez <nenrique0112@gmail.com>
2023-01-30 22:22:39 -0400

    Refactorization

--- Services/LocalMailService.cs
+++ Services/LocalMailService.cs
@@ -2,14 +2,14 @@
 
 public class LocalMailService
 {
-    private string _mailTo = "admin@mycompany.com";
-    private string _mailFrom = "noreply@mycompany.com";
+    private const string MailTo = "admin@mycompany.com";
+    private const string MailFrom = "noreply@mycompany.com";
 
     public void Send(string subject, string message)
     {
         // Send mail - output to console as we are not implementing an actual mail server
         Console.WriteLine("====================================================================");
-        Console.WriteLine($"Mail from {_mailFrom} to {_mailTo}, with {nameof(LocalMailService)}");
+        Console.WriteLine($"Mail from {MailFrom} to {MailTo}, with {nameof(LocalMailService)}");
         Console.WriteLine($"Subject: {subject}");
         Console.WriteLine($"Message: {message}");
         Console.WriteLine("====================================================================");
