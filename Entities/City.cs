0c669037fa25ffdebb4e7a92157b7c50b932f130
Enrique Núñez <nenrique0112@gmail.com>
2023-02-01 18:32:10 -0400

    Added entities and data annotations

---
+++ Entities/City.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.Api.Entities;
+
+public class City
+{
+    [Key]  // As the member has Id as name, it would automatically be the primary key
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]  // Generation of Id
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    [MaxLength(200)]
+    public string? Description { get; set; }
+
+    public ICollection<PointOfInterest> PointsOfInterest { get; set; } = new List<PointOfInterest>();
+
+    public City(string name)
+    {
+        Name = name;
+    }
+}
---
+++ Entities/PointOfInterest.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CityInfo.Api.Entities;
+
+public class PointOfInterest
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
+    [Required]
+    [MaxLength(50)]
+    public string Name { get; set; }
+    [ForeignKey("CityId")]  // Foreign key's name from navigation property in this class
+    public City? City { get; set; }  // Navigation property (foreign key) by convention a relationship will be created by the ORM
+    public int CityId { get; set; }
+
+    public PointOfInterest(string name)
+    {
+        Name = name;
+    }
+}
